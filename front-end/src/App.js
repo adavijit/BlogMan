f0e06738291372b5c58eb6a0830f690cb75cfe85
Abhishek Paul <reachtoabhishekpaul@gmail.com>
2019-10-15 21:50:25 +0530

    Sign In created

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,21 +4,90 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      const Welcome = ({user, onSignOut})=> {
+  return (
+    <div>
+      Welcome <strong>{user.username}</strong>!
+      <a href="javascript:;" onClick={onSignOut}>Sign out</a>
+    </div>
+  )
+}
+
+class LoginForm extends React.Component {
+ 
+  handleSignIn(e) {
+    e.preventDefault()
+    let username = this.refs.username.value
+    let password = this.refs.password.value
+    this.props.onSignIn(username, password)
+  }
+  
+  render() {
+    return (
+      <form onSubmit={this.handleSignIn.bind(this)}>
+        <h3>Sign in</h3>
+        <input type="text" ref="username" placeholder="enter you username" />
+        <input type="password" ref="password" placeholder="enter password" />
+        <input type="submit" value="Login" />
+      </form>
+    )
+  }
+
+}
+
+
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props)
+    // the initial application state
+    this.state = {
+      user: null
+    }
+  }
+  
+ 
+  signIn(username, password) {
+   
+    this.setState({
+      user: {
+        username,
+        password,
+      }
+    })
+  }
+  
+  signOut() {
+    
+    this.setState({user: null})
+  }
+  
+  render() {
+    
+    return (
+      <div>
+        <h1>Hello Blogger ! Please Sign IN</h1>
+        { 
+          (this.state.user) ? 
+            <Welcome 
+             user={this.state.user} 
+             onSignOut={this.signOut.bind(this)} 
+            />
+          :
+            <LoginForm 
+             onSignIn={this.signIn.bind(this)} 
+            />
+        }
+      </div>
+    )
+    
+  }
+  
+}
+
+ReactDOM.render(<App/>, document.getElementById("app"))
+
     </div>
   );
 }
