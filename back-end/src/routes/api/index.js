29852b80f6ae027c6928434aab59fbaa854f4b4e
Sebastián Blanco <sebastianabg@ext.inditex.com>
2019-10-15 13:13:37 +0200

    Create login/register functionality

---
+++ back-end/src/routes/api/index.js
@@ -0,0 +1,5 @@
+const router = require('express').Router()
+
+router.use('/users', require('./users'))
+
+module.exports = router
---
+++ back-end/src/utils/constants.js
@@ -0,0 +1,9 @@
+module.exports = Object.freeze({
+    STATUS_OK: 'OK',
+    STATUS_KO: 'KO',
+
+    INCORRECT_PASSWORD: 'Incorrect password',
+    TOKEN_ERROR: 'There is some error in token',
+    USER_NOT_FOUND: 'User not found',
+    
+})
