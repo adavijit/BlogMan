03edff6a7173f53d8516be31879b80b0fec0d2af
Pratham Dogra <43649218+PrathamDogra@users.noreply.github.com>
2020-02-25 19:52:53 +0530

    Resolves issue #13
    Both frontend and backend were running at the same ports.

--- back-end/server.js
+++ back-end/server.js
@@ -9,7 +9,7 @@ mongoose.Promise = global.Promise;
 
 const isProduction = process.env.NODE_ENV === "production";
 
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 5000);
 
 require("dotenv").config();
 
