b6ac4872c8d733b2ce04c717d526e9ff4045ba21
simranaggarwal1999 <simranaggarwal738@gmail.com>
2020-03-11 19:24:01 +0530

    Added recommender and basic algorithm using dummy data

---
+++ back-end/src/routes/api/blog.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+// const controller = require('../../controllers/api/blog');
+const recommender = require("recommender")
+// const userModel=require("../../models/User")
+
+//find user using userId
+// let userId=req.query; 
+// let user=userModel.findById(userId);
+let user={
+    postsLiked:"politcal science",
+    interests:"english",
+    comments:"hindi is really interesting",
+    skills:"Politics"
+};
+
+//users->likes, interests, comments, skills
+let query = `${user.postsLiked} ${user.interests} ${user.comments} ${user.skills}`
+console.log(query)
+
+let course1='science';
+let course2='engineering';
+let course3='english';
+let course4='Mathematics';
+let course5 = 'accounts';
+
+//new posts and new courses
+let documents = [
+    `${course1}`,
+    `${course2}`,
+    `${course3}`,
+    `${course4}`,
+    `${course5}`
+];
+console.log(documents)
+
+let sortedDocs='';
+recommender.tfidf(query, documents, (sortedDocs) => {
+    console.log(sortedDocs);
+});
+
+//send sortedDocs to Controller blog.js for displaying
+// router.get('/blogs', controller.blog);
+
+module.exports = router;
