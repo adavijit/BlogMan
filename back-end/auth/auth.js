7ce4d69bc9c1de8f49485616fd85f249d3206f12
Aditya Tomar <aditya001tomar@gmail.com>
2020-03-11 22:11:03 +0530

    Edited some files

--- back-end/auth/auth.js
+++ back-end/auth/auth.js
@@ -1,6 +1,11 @@
-
-
+var GithubStrategy = require('passport-github2').Strategy;
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+var GITHUB_CLIENT_ID='40b18d9786b1adc91eab';
+var GITHUB_CLIENT_SECRET='f5c4a752cd43fe9e9462fab0893fadbfe2998f11';
+var GOOGLE_CLIENT_ID='810189725883-jitqrtnggts7f2p2d4lrl2qbr7rrh6b4.apps.googleusercontent.com';
+var GOOGLE_CLIENT_SECRET='7KLat_4Fz31LJahbTmHoPaMq';
+
 
 module.exports = (passport) => {
     passport.serializeUser((user, done) => {
@@ -9,9 +14,11 @@ module.exports = (passport) => {
     passport.deserializeUser((user, done) => {
         done(null, user);
     });
+
+    //Google Strategy
     passport.use(new GoogleStrategy({
-            clientID: '810189725883-jitqrtnggts7f2p2d4lrl2qbr7rrh6b4.apps.googleusercontent.com',
-            clientSecret: '7KLat_4Fz31LJahbTmHoPaMq',
+            clientID: GOOGLE_CLIENT_ID,
+            clientSecret: GOOGLE_CLIENT_SECRET,
             callbackURL: '/auth/google/secrets'
         },
         (token, refreshToken, profile, done) => {
@@ -21,7 +28,8 @@ module.exports = (passport) => {
             });
         }));
 
-        passport.use(new GitHubStrategy({
+        //Github Strategy
+        passport.use(new GithubStrategy({
             clientID: GITHUB_CLIENT_ID,
             clientSecret: GITHUB_CLIENT_SECRET,
             callbackURL: "/auth/github/secrets"
