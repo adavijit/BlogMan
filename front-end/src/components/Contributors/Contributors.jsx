4de514a74cc7ad14457119826920ce2035536a3f
simranaggarwal1999 <simranaggarwal738@gmail.com>
2020-03-12 14:01:51 +0530

    Added contributors component

--- front-end/src/components/Contributors/Contributors.jsx
+++ front-end/src/components/Contributors/Contributors.jsx
@@ -1,11 +1,61 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+const { useState } = React
+
+const Card = props => {
+    return (
+        <div style={{ margin: '1em' }}>
+            <img alt="avatar" style={{ width: '70px' }} src={props.avatar_url} />
+            <div>
+                <div style={{ fontWeight: 'bold' }}>{props.name}</div>
+                <div>{props.blog}</div>
+            </div>
+        </div>
+    )
+}
+
+const CardList = props => <div>{props.cards.map(card => <Card {...card} />)}</div>
+
+const Form = props => {
+    const [username, setUsername] = useState('')
+
+    handleSubmit = event => {
+        event.preventDefault()
+
+        axios
+            .get(`https://api.github.com/users/${username}`)
+            .then(resp => {
+                props.onSubmit(resp.data)
+                setUsername('')
+            })
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                value={username}
+                onChange={event => setUsername(event.target.value)}
+                placeholder="GitHub username"
+                required
+            />
+            <button type="submit">Add card</button>
+        </form>
+    )
+}
 
 export class Contributors extends Component {
     render() {
+        const [cards, setCards] = useState([])
+
+        addNewCard = cardInfo => {
+            setCards(cards.concat(cardInfo))
+        }
         return (
-            <div>
-                
-            </div>
+                <div>
+                    <Form onSubmit={addNewCard} />
+                    <CardList cards={cards} />
+                </div>
         )
     }
 }
