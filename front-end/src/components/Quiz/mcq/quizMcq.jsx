05b949805fea0450fead9a37656ef71c411d0cbb
lakshyajit <lakshyajit165@gmail.com>
2020-03-17 22:59:21 +0530

    quizmcq component loading and route setup

---
+++ front-end/src/components/Quiz/mcq/quizMcq.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class QuizMcq extends Component {
+    render() {
+      return (
+        <div>
+            <p>Quiz mcq component!</p>
+        </div>
+      );
+    }
+  }
