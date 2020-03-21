aa502d3342f1d46213156a4bc50b1b1174ed5247
lakshyajit <lakshyajit165@gmail.com>
2020-03-21 23:23:00 +0530

    all sections of mcq quiz section complete

--- front-end/src/components/Quiz/mcq/quizMcq.jsx
+++ front-end/src/components/Quiz/mcq/quizMcq.jsx
@@ -14,6 +14,7 @@ import CardContent from '@material-ui/core/CardContent';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
@@ -29,11 +30,18 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableFooter from '@material-ui/core/TableFooter';
 import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 import ShareIcon from '@material-ui/icons/Share';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import InstagramIcon from '@material-ui/icons/Instagram';
+import LooksOneIcon from '@material-ui/icons/LooksOne';
+import LooksTwoIcon from '@material-ui/icons/LooksTwo';
+import Looks3Icon from '@material-ui/icons/Looks3';
+import Looks4Icon from '@material-ui/icons/Looks4';
+
 
 import Grid from '@material-ui/core/Grid';
 
@@ -64,8 +72,15 @@ class quizMcq extends Component {
     return (
       <div className={classes.root}>
 
-      
-          <Breadcrumbs aria-label="breadcrumb" className="breadcrumbs" style={{ marginLeft: '10px', marginRight: '10px'}}>
+          {/* Include the navbar */}
+
+          <Navbar/>
+
+
+          {/* Breadcrumbs sections */}
+
+
+          <Breadcrumbs aria-label="breadcrumb" className="breadcrumbs" style={{ marginLeft: '10px', marginRight: '10px', marginTop: '80px'}}>
             
             <Link color="inherit" href="/" onClick={handleClick} style={{ textDecoration: 'none'}}>
               Category
@@ -75,12 +90,17 @@ class quizMcq extends Component {
 
           </Breadcrumbs>  
        
+        {/* Main Grid containing the overall layout */}
+
 
         <Grid container style={{
           margin: 0,
           width: '100%',
         }} spacing={2}>
-        
+
+            {/* Topics Section */}
+
+
             <Grid item xs={12} sm={12} md={2}>
             <Card className={classes.root}>
               <CardContent>
@@ -107,6 +127,10 @@ class quizMcq extends Component {
               </CardContent>
             
             </Card>
+
+            {/* Difficulty level section */}
+
+
             <Card className={classes.root} style={{ marginTop: '10px' }}>
               <CardContent>
               <Typography gutterBottom variant="h5" component="h2">
@@ -133,6 +157,8 @@ class quizMcq extends Component {
             
             </Card>
 
+            {/* Rating Section */}
+
             <Card className={classes.root} style={{ marginTop: '10px' }}>
               <CardContent>
               <Typography gutterBottom variant="h5" component="h2">
@@ -158,10 +184,111 @@ class quizMcq extends Component {
             </Card>
       
             </Grid>
+
+            {/* Main Quiz section */}
+
             <Grid item xs={12} sm={12} md={8}>
             
-      
+              <Card className={classes.root} style={{ marginTop: '10px' }}>
+                <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                Ongoing Quiz
+                </Typography>
+                <hr/>
+                <Breadcrumbs aria-label="breadcrumb" style={{ marginLeft: '10px', marginRight: '10px'}}>
+            
+                  <Link color="inherit" href="/" onClick={handleClick} style={{ textDecoration: 'none'}}>
+                    Technology
+                  </Link>
+            
+                  <Typography color="textPrimary">MCQ</Typography>
+
+                </Breadcrumbs> 
+
+                <div className="question-section"> 
+                  <Typography variant="h4" gutterBottom>
+                      1. Which of the following cloud service providers have Cloud Spanner has one of its services?
+                    </Typography> 
+
+                    <List>
+             
+                <ListItem>
+                  <ListItemIcon>
+                    <LooksOneIcon />
+                  </ListItemIcon>
+                  <Typography variant="h5" gutterBottom>
+                      Amazon Web Services
+                  </Typography> 
+
+                </ListItem>
+
+                <ListItem className="answer">
+                  <ListItemIcon>
+                    <LooksTwoIcon />
+                  </ListItemIcon>
+                  <Typography variant="h5" gutterBottom>
+                      Google Cloud Platform
+                  </Typography> 
+
+                </ListItem>
+
+                <ListItem>
+                  <ListItemIcon>
+                    <Looks3Icon />
+                  </ListItemIcon>
+                  <Typography variant="h5" gutterBottom>
+                      Microsoft Azure
+                  </Typography> 
+
+                </ListItem>
+
+                <ListItem>
+                  <ListItemIcon>
+                    <Looks4Icon />
+                  </ListItemIcon>
+                  <Typography variant="h5" gutterBottom>
+                      Redhat Openshift
+                  </Typography> 
+
+                </ListItem>
+             
+            </List>
+                <Button variant="outlined" size="large">
+                  Submit
+                </Button>
+                </div>
+                </CardContent>
+                
+
+              </Card>
+
+
+              {/* Comments section */}
+
+              <Card className={classes.root} style={{ marginTop: '10px' }}>
+                  <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                  Comments
+                  </Typography>
+                  <hr/>
+                  <TextField
+                      
+                      label="Comment"
+                      multiline
+                      rows="4"
+                      variant="outlined"
+                     style={{ width: '100%', marginBottom: '10px'}} /><br/>
+                     <Button variant="outlined" size="large">
+                  Post
+                </Button>
+                  </CardContent>
+                
+              </Card>
             </Grid>
+
+            {/* Scoreboard section */}
+
+
             <Grid item xs={12} sm={12} md={2}>
               <Card className={classes.root} style={{ marginTop: '10px' }}>
                   <CardContent>
@@ -197,6 +324,8 @@ class quizMcq extends Component {
                   </CardContent>
               
                 </Card>
+
+                {/* Social Section */}
 
                 <Card className={classes.root} style={{ marginTop: '10px' }}>
                   <CardContent>
