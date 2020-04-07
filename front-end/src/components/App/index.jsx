d5de737903b0f6ac171406506723b87323c0ba3b
Lakshyajit <lakshyajit165@gmail.com>
2020-04-07 17:15:12 +0530

    completed coding the wireframe for userprofile3

--- front-end/src/components/App/index.jsx
+++ front-end/src/components/App/index.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, withRouter, Switch, Route } from 'react-router-dom'
 
 import { Home, Register, Login, PrivateRoute, ChatRoom , Contributors } from '..'
 import QuizMcq  from '../Quiz/mcq/quizMcq';
+import UserProfile from '../UserProfile/User3/UserProfile';
 
 import '../../styles/index.css'
 
@@ -14,6 +15,7 @@ const App = () => (
             <Route exact path="/sign-in" component={Login} />
             <Route exact path="/sign-up" component={Register} />
             <Route exact path="/quizmcq" component={QuizMcq} />
+            <Route exact path="/userprofile3" component={UserProfile} />
             <PrivateRoute exact path="/chat-room" component={ChatRoom} />
         </Switch>
     </BrowserRouter>
